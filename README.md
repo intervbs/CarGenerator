b192ecc6890cd1d4d76209b3430431c562722359
Jøran Lillegård <jli054@uit.no>
2023-02-05 21:36:20 +0100

    Fixed the the code so it stores the dictionary with cars in a file for the user to import into the code

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # CarGenerator
 Generates a list of cars that can be used in Assignments for DTE-2511
 
-This file can be used to auto generate vehicle into the database for the mandatory assignment.
-The code is hardcoded with the list name, chang this to match your list.
+This file can be used to auto generate vehicles into a dictionary, that is dumped into a file that can be used for the mandatory assignment.
+
+To read the file use this line:
+
+import pickle
+with open('car_list.txt', 'rb') as fp:
+    car_dict = pickle.load(fp)
--- generate_car_list.py
+++ generate_car_list.py
@@ -1,6 +1,6 @@
 '''Written by Jøran Lillegård 2023'''
 import random
-from vehicles import Car, Truck, SUV
+import pickle
 
 def fileToDictionary(filename):
     '''Use the complete name with file extension (text.txt)'''
@@ -11,10 +11,10 @@ def fileToDictionary(filename):
             passings.append(key[0])
     return passings
 
-def generateCars():
-    car_list = []
+def generateCars(filename):
+    car_list = {}
     # Change the name on the input file here
-    v_dict = fileToDictionary('box_a.txt')
+    v_dict = fileToDictionary(filename)
     cars = {'Volvo': {'car': {'S60', 'S90', 'V60'}, 'SUV': {'ZC40', 'XC60', 'XC90'},},
             'Volkswagen': {'truck': {'Amarok', 'Crafter Pickup', 'Transporter Pickup'}, 'car': {'Golf', 'Passat', 'Jetta'}, 'SUV': {'ID4', 'T-Cross', 'T-Roc'}},
             'Toyota': {'truck': {'Hilux', 'Tacoma', 'Tundra'}, 'car': {'Agya', 'Supra', 'Yaris'}, 'SUV': {'RAV 4', 'C-HR', 'Yaris Cross'}},
@@ -26,25 +26,30 @@ def generateCars():
             'Mercedes-Benz': {'car': {'CLA', 'CLS', 'EQE'}, 'SUV': {'EQE SUV', 'EQA', 'EQB'}},
             'Peugeot': {'truck': {'Landtrek'}, 'car':{'208', '301', '308'}, 'SUV': {'3008', '2008', '5008'}}
             }
+    no = 0
     for regno in v_dict:
+        no += 1
         # Makes random choises for the brand, model and type
         brand = random.choice(list(cars))
         v_type = random.choice(list(cars[brand]))
         car = random.choice(list(cars[brand][v_type]))
         # Appending the choices into the correct class object
         if v_type == 'car':
-            car_list.append(Car(f'{brand} {car}', random.randint(2011, 2023), 
-                                    random.randint(0, 200000), random.randint(15000, 900000), 
-                                    random.choice([2, 4]), regno))
+            car_list[no] = {'brand': f'{brand} {car}', 'year': random.randint(2011, 2023), 
+                        'km': random.randint(0, 200000), 'price': random.randint(15000, 900000), 
+                        'doors': random.choice([2, 4]), 'plate': regno}
         elif v_type == 'truck':
-            car_list.append(Truck(f'{brand} {car}', random.randint(2011, 2023), 
-                                    random.randint(0, 200000), random.randint(15000, 900000), 
-                                    random.choice(['2WD', '4WD']), regno))
+            car_list[no] = {'brand': f'{brand} {car}', 'year': random.randint(2011, 2023), 
+                        'km': random.randint(0, 200000), 'price': random.randint(15000, 900000), 
+                        'drive': random.choice(['2WD', '4WD']), 'plate': regno}
         elif v_type == 'SUV':
-            car_list.append(SUV(f'{brand} {car}', random.randint(2011, 2023), 
-                                    random.randint(0, 200000), random.randint(15000, 900000), 
-                                    random.choice([2, 3, 4]), regno))
+            car_list[no] = {'brand': f'{brand} {car}', 'year': random.randint(2011, 2023), 
+                        'km': random.randint(0, 200000), 'price': random.randint(15000, 900000), 
+                        'seats': random.choice([2, 3, 4]), 'plate': regno}
         else:
             pass
-        
     return car_list
+if __name__ == '__main__':        
+    car_list = generateCars(input('Enter Filename >>> '))
+    with open('car_list.txt', 'wb') as fp:
+        pickle.dump(car_list, fp)
